c3326058ec3b8a70306fdba1ed86124028084d06
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-01-15 16:42:15 +0600

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
 import Image from "next/image";
+import Navbar from '@/app/components/navbar';
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar/>
+      <hr />
     </div>
+    // <div className="navbar bg-base-100">
+    //   <div className="flex-1">
+    //     <a className="btn btn-ghost text-xl">daisyUI</a>
+    //   </div>
+    //   <div className="flex-none">
+    //     <ul className="menu menu-horizontal px-1">
+    //       <li>
+    //         <a>Link</a>
+    //       </li>
+    //       <li>
+    //         <details>
+    //           <summary>Parent</summary>
+    //           <ul className="bg-base-100 rounded-t-none p-2">
+    //             <li>
+    //               <a>Link 1</a>
+    //             </li>
+    //             <li>
+    //               <a>Link 2</a>
+    //             </li>
+    //           </ul>
+    //         </details>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
   );
 }
