7daeb2484bb5ecb4562a54c74d3e7ae3de743219
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-01-30 14:11:05 +0600

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/:path*', // Proxy to backend
+      },
+    ];
+  },
 };
-
-export default nextConfig;
