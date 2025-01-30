a117be147bcf9bd586ae7a454183b7c67c0eb18b
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-01-30 14:09:34 +0600

    Add footer component

---
+++ app/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>
+      <footer className="bg-gray-100 py-6 text-center">
+        <p className="text-sm text-gray-500">
+          Copyright &copy; Culinary Odyssey 2024
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default footer
